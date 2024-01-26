6083b8a6542e72a65635bb8469be1d1a7f4b9fe0
Karim Huesmann <karimhuesmann@me.com>
2024-01-26 13:58:13 +0100

    Added R file for building

---
+++ R/internal.R
@@ -0,0 +1,15 @@
+.ppc_js_metadata <- function() {
+deps_metadata <- list(`ppc` = structure(list(name = "ppc",
+version = "1.0.0", src = list(href = NULL,
+file = "deps"), meta = NULL,
+script = 'ppc.js',
+stylesheet = NULL, head = NULL, attachment = NULL, package = "ppc",
+all_files = FALSE), class = "html_dependency"),
+`ppc` = structure(list(name = "ppc",
+version = "1.0.0", src = list(href = NULL,
+file = "deps"), meta = NULL,
+script = 'ppc.js.map',
+stylesheet = NULL, head = NULL, attachment = NULL, package = "ppc",
+all_files = FALSE, dynamic = TRUE), class = "html_dependency"))
+return(deps_metadata)
+}
