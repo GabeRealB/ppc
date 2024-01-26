b308f9b923dbcb63bdf71229a286a92474b8f481
Karim Huesmann <karimhuesmann@me.com>
2024-01-26 13:58:35 +0100

    Updated usage.py to show a demo use of the PCC component via Dash

--- usage.py
+++ usage.py
@@ -1,10 +1,58 @@
 import ppc
+import random
 import dash
+from dash import html
 
-app = dash.Dash(__name__)
+LINE_COUNT = 100
 
-app.layout = ppc.PPC(id='component')
+app = dash.Dash(__name__, assets_folder='deps')
 
+props = {
+    "axes": {
+        "a1": {
+            "label": "A1",
+            "range": [0, 10],
+            "dataPoints": [random.random() * 10 for _ in range(LINE_COUNT)]
+        },
+        "a2": {
+            "label": "A2",
+            "range": [0, 10],
+            "dataPoints": [random.random() * 10 for _ in range(LINE_COUNT)]
+        },
+        "a3": {
+            "label": "A3",
+            "range": [0, 10],
+            "dataPoints": [random.random() * 10 for _ in range(LINE_COUNT)]
+        }
+    },
+    "order": ["a1", "a2", "a3"],
+    "labels": {
+        "Default": {},
+    },
+    "activeLabel": "Default",
+    "colors": {
+        "selected": {
+            "scale": "plasma",
+            "color": 0.5,
+        }
+    },
+    "colorBar": "hidden",
+    "interactionMode": 2,
+    "debug": {
+        "showAxisBoundingBox": False,
+        "showLabelBoundingBox": False,
+        "showCurvesBoundingBox": False,
+        "showAxisLineBoundingBox": False,
+        "showSelectionsBoundingBox": False,
+        "showColorBarBoundingBox": False,
+    }
+}
+
+app.layout = html.Div([
+    ppc.PPC(id='component', **props)
+], style={
+    "height": "1000px"
+})
 
 if __name__ == '__main__':
     app.run_server(debug=True)
