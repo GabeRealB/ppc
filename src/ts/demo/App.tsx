157420d6a6db3d4689e7ebd22e68962d68cf1e01
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2024-01-30 19:26:44 +0100

    Add welcome and questionnaire pages

--- src/ts/demo/App.tsx
+++ src/ts/demo/App.tsx
@@ -1,5 +1,5 @@
 /* eslint no-magic-numbers: 0 */
-import React, { Component } from 'react';
+import React, { Component, createElement, useState } from 'react';
 import { v4 as uuid } from 'uuid';
 
 import List from '@mui/material/List';
@@ -36,6 +36,12 @@ import DialogTitle from '@mui/material/DialogTitle';
 import Skeleton from '@mui/material/Skeleton';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Unstable_Grid2';
+import Alert from '@mui/material/Alert';
+import Checkbox from '@mui/material/Checkbox';
+import Select from '@mui/material/Select';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Rating from '@mui/material/Rating';
 
 import HelpIcon from '@mui/icons-material/Help';
 import RestartAltIcon from '@mui/icons-material/RestartAlt';
@@ -51,6 +57,7 @@ import OpenWithIcon from '@mui/icons-material/OpenWith';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import StopIcon from '@mui/icons-material/Stop';
 import InfoIcon from '@mui/icons-material/Info';
+import StarIcon from '@mui/icons-material/Star';
 
 import moveAxesInstr from './resources/move_axes_instr.mp4'
 
@@ -71,7 +78,38 @@ type DemoTask = {
 
 type UserGroup = "PC" | "PPC";
 
+type DemoPage = "welcome"
+    | "demo1"
+    | "demo2"
+    | "demo3"
+    | "finish";
+
+type LevelOfEducation = "Childhood"
+    | "Primary"
+    | "LowerSecondary"
+    | "UpperSecondary"
+    | "Post-secondary"
+    | "Tertiary"
+    | "Bachelor"
+    | "Master"
+    | "Doctoral"
+
+type Proficiency = "NA"
+    | "Fundamental"
+    | "Novice"
+    | "Intermediate"
+    | "Advanced"
+    | "Expert"
+
+type Results = {
+    education?: LevelOfEducation,
+    analysisProficiency?: Proficiency,
+    pcProficiency?: Proficiency,
+};
+
 type DemoState = {
+    currentPage: DemoPage,
+
     userId: uuid,
     userGroup: UserGroup,
 
@@ -80,6 +118,8 @@ type DemoState = {
     showInstructions: boolean,
 
     showDebugInfo: boolean,
+
+    results: Results,
 };
 
 type AppState = {
@@ -107,12 +147,14 @@ class App extends Component<any, AppState> {
         this.state = {
             ppcState: ppc,
             demo: {
+                currentPage: "welcome",
                 userId: uuid(),
                 userGroup: userGroup as UserGroup,
                 showInstructions: true,
                 currentTask: 0,
                 tasks: tasks,
-                showDebugInfo: debugMode
+                showDebugInfo: debugMode,
+                results: {}
             },
         };
     }
@@ -131,51 +173,345 @@ class App extends Component<any, AppState> {
     }
 
     render() {
-        const {
-            ppcState,
-            demo,
-        } = this.state;
-        const {
-            axes,
-        } = ppcState;
+        let page = null;
+        switch (this.state.demo.currentPage) {
+            case 'welcome':
+                page = createElement(WelcomePage, this);
+                break;
+            case 'demo1':
+                page = createElement(DemoPage1, this);
+                break;
+            case 'demo3':
+                page = DemoPage3(this);
+                break;
+            case 'finish':
+                break;
+        }
 
-        return (
-            <Box style={{ height: "95%", padding: "2rem" }}>
-                <Grid container style={{ height: "100%" }} spacing={2}>
-                    <Grid xs={10}>
-                        <PPC
-                            {...ppcState}
-                        />
-                    </Grid>
-                    <Grid xs={2} maxHeight={"95%"} sx={{ overflow: "auto" }}>
-                        <Stack
-                            spacing={2}
-                            justifyContent="flex-start"
-                            alignItems="flex-start"
-                            paddingX={"2rem"}
-                        >
-                            {InstructionsDialog(demo, this.setProps)}
-
-                            {TaskView(ppcState, demo, this.setProps)}
-                            <Divider flexItem />
-                            {LabelsView(ppcState, demo, this.setProps)}
-                            <Divider flexItem />
-
-                            <div>
-                                {AttributeList(ppcState, axes, this.setProps)}
-                                {ColorSettings(ppcState, demo, this.setProps)}
-                                {ActionsInfo(ppcState)}
-                                {DebugInfo(ppcState, demo, this.setProps)}
-                            </div>
-                        </Stack>
-                    </Grid>
-                </Grid>
-            </Box>
-        )
+        return page;
     }
 }
 
 export default App;
+
+function WelcomePage(app: App) {
+    const hasWebgpu = !!navigator.gpu;
+    const isMobile = (() => {
+        let check = false;
+        (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || (window as any).opera);
+        return check;
+    })();
+    const isChrome = navigator.userAgent.includes("Chrome");
+
+    let statusElement = null;
+    if (!hasWebgpu) {
+        statusElement = <Alert severity="error">
+            Your browser does not support WebGPU, which is required for this test.
+            Supported browsers are Google Chrome and Microsoft Edge.
+        </Alert>;
+    } else if (isMobile) {
+        statusElement = <Alert severity="error">
+            This test is not supported on mobile browsers.
+        </Alert>;
+    } else if (!isChrome) {
+        statusElement = <Alert severity="warning">
+            This test has only been tested on Google Chrome and Microsoft Edge.
+            Other browsers may not be able to display the contents properly.
+        </Alert>;
+    } else {
+        statusElement = <Alert severity="success">
+            All prerequisites for the test are met.
+        </Alert>;
+    }
+
+    const [accepted, setAccepted] = useState<boolean>(false);
+    const canStart = hasWebgpu && !isMobile && accepted;
+
+    const handleChecked = (e, checked) => {
+        setAccepted(checked);
+    }
+
+    const handleClick = () => {
+        const { demo } = app.state;
+        demo.currentPage = "demo1";
+        app.setProps({ demo });
+    }
+
+    const dataAcquisitionTerms = `
+    This test requires the collection of anonymized user data, consisting of 
+    timing information, interactions and responses, for the sake of evaluation.
+    `;
+
+    return (
+        <Container style={{ height: "95%", padding: "2rem" }}>
+            <Typography variant='h2'><b>Welcome</b></Typography>
+
+            <Typography variant='body1' marginY={2}>
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                justo duo dolores et ea rebum. Stet clita kasd gubergren, no
+                sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem
+                ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+                nonumy eirmod tempor invidunt ut labore et dolore magna
+                aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                justo duo dolores et ea rebum. Stet clita kasd gubergren, no
+                sea takimata sanctus est Lorem ipsum dolor sit amet.
+            </Typography>
+
+            <Typography variant="subtitle1" marginTop={5} marginBottom={1}>
+                <b>{dataAcquisitionTerms}</b>
+            </Typography>
+
+            <FormGroup>
+                <FormControlLabel
+                    required
+                    control={<Checkbox value={accepted} onChange={handleChecked} />}
+                    label="I consent to the collection of anonymized user data."
+                />
+            </FormGroup>
+
+            <Box marginY={2}>
+                {statusElement}
+            </Box>
+
+            <Container>
+                <Box marginY={2}>
+                    <Button
+                        variant="contained"
+                        onClick={handleClick}
+                        fullWidth
+                        disabled={!canStart}
+                    >
+                        Start test
+                    </Button>
+                </Box>
+            </Container>
+        </Container>
+    );
+}
+
+function DemoPage1(app: App) {
+    const { results } = app.state.demo;
+
+    const [education, setEducation] = useState<LevelOfEducation>(undefined);
+    const [analysisProficiency, setAnalysisProficiency] = useState<number>(undefined);
+    const [pcProficiency, setPcProficiency] = useState<number>(undefined);
+
+    const handleEducationChange = (e) => {
+        results.education = e.target.value as LevelOfEducation;
+        setEducation(e.target.value as LevelOfEducation);
+    }
+
+    const handleAnalysisProficiencyChange = (e, proficiency) => {
+        setAnalysisProficiency(proficiency);
+        switch (proficiency) {
+            case 1:
+                results.analysisProficiency = "NA";
+                break;
+            case 2:
+                results.analysisProficiency = "Fundamental";
+                break;
+            case 3:
+                results.analysisProficiency = "Novice";
+                break;
+            case 4:
+                results.analysisProficiency = "Intermediate";
+                break;
+            case 5:
+                results.analysisProficiency = "Advanced";
+                break;
+            case 6:
+                results.analysisProficiency = "Expert";
+                break;
+        }
+    }
+
+    const handlePcProficiencyChange = (e, proficiency) => {
+        setPcProficiency(proficiency);
+        switch (proficiency) {
+            case 1:
+                results.pcProficiency = "NA";
+                break;
+            case 2:
+                results.pcProficiency = "Fundamental";
+                break;
+            case 3:
+                results.pcProficiency = "Novice";
+                break;
+            case 4:
+                results.pcProficiency = "Intermediate";
+                break;
+            case 5:
+                results.pcProficiency = "Advanced";
+                break;
+            case 6:
+                results.pcProficiency = "Expert";
+                break;
+        }
+    }
+
+    const handleClick = () => {
+        const { demo } = app.state;
+        demo.currentPage = "demo3";
+        app.setProps({ demo });
+    }
+
+    const proficiencyLabels: { [index: string]: string } = {
+        1: "No experience",
+        2: "Fundamental",
+        3: "Novice",
+        4: "Intermediate",
+        5: "Advanced",
+        6: "Expert",
+    };
+
+    const canContinue = education !== undefined
+        && analysisProficiency !== undefined
+        && pcProficiency !== undefined;
+
+    return (
+        <Container>
+            <Typography variant="h4">
+                <b>Thank you for your interest in participating in this study.</b>
+            </Typography>
+            <Typography variant="body1" marginY={1}>
+                Before we begin with the interactive test, we would like to ask you
+                to fill out the short questionnaire below.
+            </Typography>
+
+            <Divider />
+
+            <Typography variant="subtitle1" marginY={2}>
+                What is your level of education?
+            </Typography>
+            <FormControl>
+                <InputLabel id="education-label">Level of education</InputLabel>
+                <Select
+                    labelId="education-label"
+                    id="education-select"
+                    value={education}
+                    label="Education"
+                    sx={{ m: 1, minWidth: 240 }}
+                    onChange={handleEducationChange}
+                >
+                    <MenuItem value="Childhood">Early childhood Education</MenuItem>
+                    <MenuItem value="Primary">Primary education</MenuItem>
+                    <MenuItem value="LowerSecondary">Lower secondary education</MenuItem>
+                    <MenuItem value="UpperSecondary">Upper secondary education</MenuItem>
+                    <MenuItem value="Post-secondary">Post-secondary non-tertiary education</MenuItem>
+                    <MenuItem value="Tertiary">Short-cycle tertiary education</MenuItem>
+                    <MenuItem value="Bachelor">Bachelor or equivalent</MenuItem>
+                    <MenuItem value="Master">Master or equivalent</MenuItem>
+                    <MenuItem value="Doctoral">Doctoral or equivalent</MenuItem>
+                </Select>
+            </FormControl>
+
+            <Typography variant="subtitle1" marginY={2}>
+                How would you describe your proficiency in the task of data analysis?
+            </Typography>
+            <Box
+                margin={1}
+                sx={{
+                    width: 400,
+                    display: 'flex',
+                    alignItems: 'center',
+                }}
+            >
+                <Rating
+                    name="analysis-proficiency"
+                    value={analysisProficiency}
+                    max={6}
+                    size="large"
+                    onChange={handleAnalysisProficiencyChange}
+                    emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
+                />
+                <Box sx={{ ml: 2 }}>{proficiencyLabels[analysisProficiency]}</Box>
+            </Box>
+
+
+
+            <Typography variant="subtitle1" marginY={2}>
+                How would you describe your proficiency with parallel coordinates?
+            </Typography>
+            <Box
+                margin={1}
+                sx={{
+                    width: 400,
+                    display: 'flex',
+                    alignItems: 'center',
+                }}
+            >
+                <Rating
+                    name="pc-proficiency"
+                    value={analysisProficiency}
+                    max={6}
+                    size="large"
+                    onChange={handlePcProficiencyChange}
+                    emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
+                />
+                <Box sx={{ ml: 2 }}>{proficiencyLabels[pcProficiency]}</Box>
+            </Box>
+
+            <Container>
+                <Box marginY={4}>
+                    <Button
+                        variant="contained"
+                        onClick={handleClick}
+                        fullWidth
+                        disabled={!canContinue}
+                    >
+                        Next
+                    </Button>
+                </Box>
+            </Container>
+        </Container>
+    )
+}
+
+function DemoPage3(app: App) {
+    const {
+        ppcState,
+        demo,
+    } = app.state;
+    const {
+        axes,
+    } = ppcState;
+
+    return (
+        <Box style={{ height: "95%", padding: "2rem" }}>
+            <Grid container style={{ height: "100%" }} spacing={2}>
+                <Grid xs={10}>
+                    <PPC
+                        {...ppcState}
+                    />
+                </Grid>
+                <Grid xs={2} maxHeight={"95%"} sx={{ overflow: "auto" }}>
+                    <Stack
+                        spacing={2}
+                        justifyContent="flex-start"
+                        alignItems="flex-start"
+                        paddingX={"2rem"}
+                    >
+                        {InstructionsDialog(demo, app.setProps)}
+
+                        {TaskView(ppcState, demo, app.setProps)}
+                        <Divider flexItem />
+                        {LabelsView(ppcState, demo, app.setProps)}
+                        <Divider flexItem />
+
+                        <div>
+                            {AttributeList(ppcState, axes, app.setProps)}
+                            {ColorSettings(ppcState, demo, app.setProps)}
+                            {ActionsInfo(ppcState)}
+                            {DebugInfo(ppcState, demo, app.setProps)}
+                        </div>
+                    </Stack>
+                </Grid>
+            </Grid>
+        </Box>
+    )
+}
 
 const InstructionsDialog = (demo: DemoState, setProps: (newProps) => void) => {
     const { showInstructions, currentTask, tasks } = demo;
