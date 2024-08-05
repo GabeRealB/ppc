c44e0e3bf2d62638600d306935c6d2f057a31d8b
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2024-08-05 15:33:09 +0200

    Start design of the second user study

--- src/ts/demo/datasets.ts
+++ src/ts/demo/datasets.ts
@@ -1,10 +1,5 @@
 import { createSyntheticTestDataset } from './datasets/synthetic_test';
-import { createSyntheticDataset } from './datasets/synthetic';
-import { createAdultDataset } from './datasets/adult';
-import { createAblationDataset } from './datasets/ablation';
-
-import { createIrisDataset } from './datasets/iris';
-import { createShipDataset } from './datasets/ship';
+import { createApplicationDataset } from './datasets/application';
 
 import { Props } from 'types';
 
@@ -22,9 +17,9 @@ export function syntheticTestDataset(visible: string[], include: string[], sampl
     };
 }
 
-export function syntheticDataset(visible: string[], include: string[], samples?: number): { state: Props, sampleIndices: number[] } {
+export function applicationDataset(visible: string[], include: string[], samples?: number): { state: Props, sampleIndices: number[] } {
     const included = new Set([...visible, ...include]);
-    const { dataset, sampleIndices } = createSyntheticDataset(Array.from(included), samples);
+    const { dataset, sampleIndices } = createApplicationDataset(Array.from(included), samples);
     const state = {
         axes: dataset,
         order: visible,
@@ -34,54 +29,4 @@ export function syntheticDataset(visible: string[], include: string[], samples?:
     return {
         state, sampleIndices
     };
-}
-
-export function adultDataset(visible: string[], include: string[], samples?: number): { state: Props, sampleIndices: number[] } {
-    const included = new Set([...visible, ...include]);
-    const { dataset, sampleIndices } = createAdultDataset(Array.from(included), samples);
-    const state = {
-        axes: dataset,
-        order: visible,
-        labels: {},
-        setProps: undefined
-    };
-    return {
-        state, sampleIndices
-    };
-}
-
-export function ablationDataset(visible: string[], include: string[], samples?: number): { state: Props, sampleIndices: number[] } {
-    const included = new Set([...visible, ...include]);
-    const { dataset, sampleIndices } = createAblationDataset(Array.from(included), samples);
-    const state = {
-        axes: dataset,
-        order: visible,
-        labels: {},
-        setProps: undefined
-    };
-    return {
-        state, sampleIndices
-    };
-}
-
-export function irisDataset(): Props {
-    const dataset = createIrisDataset();
-    const state = {
-        axes: dataset,
-        order: [],
-        labels: {},
-        setProps: undefined
-    };
-    return state;
-}
-
-export function shipDataset(): Props {
-    const dataset = createShipDataset();
-    const state = {
-        axes: dataset,
-        order: [],
-        labels: {},
-        setProps: undefined
-    };
-    return state;
 }
